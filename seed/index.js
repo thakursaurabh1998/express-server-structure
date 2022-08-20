87c097bbdefa12e1f74b9171ea2c25e1f915e085
Saurabh Thakur <mail@saurabhthakur.dev>
2022-08-21 02:13:28 +0530

    Add seed data file

---
+++ seed/index.js
@@ -0,0 +1,37 @@
+import { sequelize } from '../utils/connections';
+import { logger } from '../utils/logger';
+
+const Users = [
+    { name: 'Saurabh Thakur', userName: 'thakursaurabh98' },
+    { name: 'Rohan Joshi', userName: 'joshi_rohan' },
+    { name: 'Lewis Hamilton', userName: 'hamilton_goat' },
+    { name: 'Daniel Ricciardo', userName: 'ritchiardo' },
+    { name: 'Mindy Kaling', userName: 'mindy_kaling' }
+];
+
+const Comments = [
+    { content: 'Awesome stuff!!', userId: 1, postId: 1 },
+    { content: 'Awesome stuff!!', userId: 2, postId: 1 },
+    { content: 'Awesome stuff!!', userId: 3, postId: 1 },
+    { content: 'Awesome stuff!!', userId: 4, postId: 1 },
+    { content: 'Awesome stuff G!!', userId: 5, postId: 1 },
+    { content: 'Awesome stuff!!', userId: 2, postId: 2 },
+    { content: 'Awesome stuff!!', userId: 2, postId: 2 }
+];
+
+export async function fillData() {
+    await Promise.all(
+        Users.map((user) =>
+            sequelize.models.User.build(user)
+                .save()
+                .catch((err) => logger.warn(err.message))
+        )
+    );
+    await Promise.all(
+        Comments.map((comment) =>
+            sequelize.models.Comment.build(comment)
+                .save()
+                .catch((err) => logger.warn(err.message))
+        )
+    );
+}
